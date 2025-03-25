9bbb3d0a0847b615c2281a221b29126f800d2e4a
JesusSantos99 <jesus.santos@ujed.mx>
2025-03-25 11:25:35 -0600

    integracion del dasboard front

--- resources/js/admin/components/dashboard/Admin.vue
+++ resources/js/admin/components/dashboard/Admin.vue
@@ -2,31 +2,61 @@
     <div class="admin-container">
         <Navbar @toggle-sidebar="toggleSidebar" />
         <div class="admin-main">
-            <Sidebar :showSidebar="showSidebar" @close-sidebar="showSidebar = false" />
+            <sidebar-menu 
+                    :menu="menu"
+                    :relative="true"
+                >
+            </sidebar-menu>
             <div class="admin-main__content">
-                <Profesores />
+                <slot>
+
+                </slot>
             </div>
         </div>
     </div>
 </template>
 <script>
-
     import Navbar from './Navbar.vue';
     import Sidebar from './Sidebar.vue';
-    import Profesores from '../Profesores.vue';
+    import { SidebarMenu } from 'vue-sidebar-menu'
+    import 'vue-sidebar-menu/dist/vue-sidebar-menu.css'
 
     export default {
 		name: 'admin',
 
         components: {
             Navbar,
-            Sidebar,
-            Profesores
+            SidebarMenu
         },
 
         data() {
             return {
-                showSidebar: false
+                showSidebar: false,
+                menu: [
+                    {
+                        header: 'S.I.G',
+                        hiddenOnCollapse: true
+                    },
+                    {
+                        href: '/',
+                        title: 'Unidades',
+                        icon: 'bi bi-shield-minus'
+                    },
+                    {
+                        title: 'Profesores',
+                        icon: 'bi bi-shield-plus',
+                        child: [
+                            {
+                                href: '/charts/sublink',
+                                title: 'Profesor'
+                            },
+                            {
+                                href: '/charts/sublink',
+                                title: 'Profesor'
+                            }
+                        ]
+                    }
+                ]
             };
         },
         methods: {
